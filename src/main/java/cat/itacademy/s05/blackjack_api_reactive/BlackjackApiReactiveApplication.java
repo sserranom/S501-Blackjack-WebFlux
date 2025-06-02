e7b7339a852c3d0bc73fadf548f6c8e34e6f7579
sserranom <sergiomelgara@gmail.com>
2025-06-02 23:12:32 +0200

    Initial project

---
+++ src/main/java/cat/itacademy/s05/blackjack_api_reactive/BlackjackApiReactiveApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s05.blackjack_api_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackApiReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackApiReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/itacademy/s05/blackjack_api_reactive/BlackjackApiReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.itacademy.s05.blackjack_api_reactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackjackApiReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
